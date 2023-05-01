feab3e768d0c138ab93ca3dee628e48a46969940
Trump2022 <qsw2012@outlook.com>
2023-05-01 23:24:16 +0800

    用户可以创建话题

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -30,6 +30,12 @@
 
         @else
 
+          <li class="nav-item">
+            <a class="nav-link mt-1 mr-3 font-weight-bold" href="{{ route('topics.create') }}">
+              <i class="fa-solid fa-plus"></i>
+            </a>
+          </li>
+
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
                aria-haspopup="true" aria-expanded="false">
