6de631cbbc7193bb327be1510e55ea4656909f88
Trump2022 <qsw2012@outlook.com>
2023-05-03 20:27:34 +0800

    消息通知有新回复

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -7,6 +7,8 @@ use App\Models\Reply;
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
+use App\Notifications\TopicReplied;
+
 class ReplyObserver
 {
     public function created(Reply $reply)
@@ -14,6 +16,9 @@ class ReplyObserver
 //        $reply->topic->increment('reply_count', 1);
         $reply->topic->reply_count = $reply->topic->replies->count();
         $reply->topic->save();
+
+        // 通知话题作者有新的评论
+        $reply->topic->user->notify(new TopicReplied($reply));
     }
 
     public function creating(Reply $reply)
